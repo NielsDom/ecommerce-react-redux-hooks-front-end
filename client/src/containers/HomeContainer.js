66fc2d9339675dbd0f0aaded514af00872f4707b
Niels Dominguez <niels.dominguez@gmail.com>
2019-05-27 22:43:15 +0700

    tests

--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Query } from "react-apollo"
+import { QUERY_EXAMPLE } from '../graphql/queries/books.js'
+
+const Yeah = () => (
+  <Query
+    query={QUERY_EXAMPLE}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return data.books.map(x=><p>{x.title}</p>)
+    }}
+  </Query>
+);
+
 
 const HomeContainer = () => {
   return (
     <div>
-      hi
+      <Yeah />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,17 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/storeConfig'
 import Router from './Router'
+import { client } from './graphql/config.js'
+import { ApolloProvider, Query } from "react-apollo"
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   </Provider>, document.getElementById('root'));
 
 
